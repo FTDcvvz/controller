8a5ecd07f881aaf4fb132c1fe84130d463b1d82f
chenwz <ftdchenwz@gmail.com>
2016-07-13 23:27:47 -0700

    finished version.

--- controller.c
+++ controller.c
@@ -9,11 +9,10 @@
 #include <pthread.h>
 #include "ruletables.h"
 #include <arpa/inet.h>
-#include <pthread.h>
 
-#define MESSAGEPORT 5555  
 #define SERVERPORT 3333  //server
 #define CONNPORT 6666     //client
+#define MESSAGEPORT 5555 
 #define MY_MOD_ADDR "127.0.0.1"
 #define MAX_PRIORITY 100	//operate_tables.c also defined
 
@@ -33,19 +32,23 @@ struct handle_c{
     ruletable table;
 }; 
 
-static void 
+static int 
 do_init(struct handle_c * handle, command_list command,int index, struct list_head p,
-	char saddr[],char daddr[],char smsk[], char dmsk[],
+	const char* saddr,const char* daddr,const char* smsk,const char* dmsk,
 	uint16_t spts0,uint16_t spts1,uint16_t dpts0,uint16_t dpts1,
-	int priority,ProtoType proto,ActionType actionType,
-	ActionDesc actionDesc,table_name tablename)
+	int priority,const char * proto,const char* actionType,
+	const char* actionDesc,const char* tablename)
 {
 	
 	handle->command = command;
 	handle->index = index;
 	handle->table.list = p;
-	handle->table.head.s_addr =  inet_addr(saddr);
-	handle->table.head.d_addr =  inet_addr(daddr);
+	if((handle->table.head.s_addr =  inet_addr(saddr))  == INADDR_NONE||
+	(handle->table.head.d_addr =  inet_addr(daddr)) == INADDR_NONE)
+	{
+		fprintf(stderr,"fault ip address.\n");
+		return 0;
+	}
 	handle->table.head.smsk =  inet_addr(smsk);
 	handle->table.head.dmsk =  inet_addr(dmsk);
 	handle->table.head.spts[0] = spts0;
@@ -53,71 +56,115 @@ do_init(struct handle_c * handle, command_list command,int index, struct list_he
 	handle->table.head.dpts[0] = dpts0;
 	handle->table.head.dpts[1] = dpts1;
 	handle->table.priority = priority;
-	handle->table.head.proto = proto;
-	handle->table.actionType = actionType;
- 	handle->table.actionDesc = actionDesc;
-	handle->table.property.tablename = tablename;
+	if(proto == NULL)
+		handle->table.head.proto = PROTO_NONE;
+	else if(strcmp(proto,"tcp") == 0)
+		handle->table.head.proto = TCP;
+	else if(strcmp(proto,"udp") == 0)
+		handle->table.head.proto = UDP;
+	else if(strcmp(proto,"icmp") == 0)
+		handle->table.head.proto = ICMP;
+	else if(strcmp(proto,"arp") == 0)
+		handle->table.head.proto = ARP;
+	
+	if(actionType == NULL) handle->table.actionType = TYPE_NONE;
+	   else  if(strcmp(actionType,"INPUT") == 0)
+		handle->table.actionType = INPUT;
+	    else if(strcmp(actionType,"OUTPUT") == 0)
+		handle->table.actionType = OUTPUT;
+	    else if(strcmp(actionType,"FORWARD") == 0)
+		handle->table.actionType = FORWARD;
+	    else if(strcmp(actionType,"PREROUTING") == 0)
+		handle->table.actionType = PREROUTING;
+	    else if(strcmp(actionType,"POSTROUTING") == 0)
+		handle->table.actionType = POSTROUTING;
 	    
+ 	    if(actionDesc== NULL) handle->table.actionDesc = DESC_NONE;
+	    else if(strcmp(actionDesc,"ACCEPT") == 0)
+		handle->table.actionDesc = ACCEPT;
+	    else if(strcmp(actionDesc,"DROP") == 0)
+		handle->table.actionDesc = DROP;
+	    else if(strcmp(actionDesc,"QUEUE") == 0)
+		handle->table.actionDesc = QUEUE;
+	    else if(strcmp(actionDesc,"RETURN") == 0)
+		handle->table.actionDesc = RETURN;
+	   
+	    if(tablename == NULL) handle->table.property.tablename = NAME_NONE;
+	    else if(strcmp(tablename,"filter") == 0)
+		handle->table.property.tablename = filter;
+	    else if(strcmp(tablename,"nat") == 0)
+		handle->table.property.tablename = nat;
+	    else if(strcmp(tablename,"mangle") == 0)
+		handle->table.property.tablename = mangle;
+	    return 1;
 }
 
-static void 
+static int 
 init_handle(struct handle_c * handle)
 {	
-	command_list command;
-	int index;
-	char sip[20],dip[20],smask[20],dmask[20];
-	uint16_t spts0,spts1,dpts0,dpts1;
-	int priority;
-	ProtoType proto;
-	ActionType actionType;
-	ActionDesc actionDesc;
-	table_name tablename;
 	struct list_head p;
-		p.prev = p.next = NULL;
-		printf("输入信息构造发送数据:\n");
-		printf("command: 0-SET_POLICY,1-APPEND,2-ALTER,3-DELETE,4-CLEAN,5-ALLIN"); 
-		scanf("%d",&command);
-		printf("index: ");
-		scanf("%d",&index);
-		printf("s ip: ");
-		scanf("%s",sip);
-		printf("d ip: ");
-		scanf("%s",dip);
-		printf("s mask : ");
-		scanf("%s",smask);
-		printf("d mask : ");
-		scanf("%s",dmask);
-		printf("s port low :");
-		scanf("%d",&spts0);
-		printf("s port high :");
-		scanf("%d",&spts1);
-		printf("d port low :");
-		scanf("%d",&dpts0);
-		printf("d port high :");
-		scanf("%d",&dpts1);
-		printf("priority : (MAX 100)");
-		scanf("%d",&priority);
-		printf("protocol:0-PROTO_NONE,1-TCP,2-UDP,3-ARP,4-ICMP\n");
-		scanf("%d",&proto);
-		printf("action type:0-TYPE_NONE,1-PREROUTING, 2-INPUT,3-OUTPUT,4-FORWARD,5-POSTROUTING\n");
-		scanf("%d",&actionType);
-		printf("action description:0-DESC_NONE,1-ACCEPT,2-DROP,3-QUEUE,4-RETURN\n");
-		scanf("%d",&actionDesc);
-		printf("tablename:0-NAME_NONE,1-filter,2-nat,3-mangle\n");
-		scanf("%d",&tablename);
-
-		do_init(handle,command,index,p,sip,dip,smask,dmask,spts0,spts1,dpts0,dpts1,priority,
-proto,actionType,actionDesc,tablename);
+	p.prev = p.next = NULL;
+	int i;
+	printf(">>>");
+	scanf("%d",&i);
+switch(i){
+	case 0:
+	return do_init(handle,SET_POLICY,0,p,"0.0.0.0","0.0.0.0","0.0.0.0",
+		"0.0.0.0",0,0,0,0,0,NULL,"FORWARD","DROP","filter");
+	case 1:
+	return do_init(handle,APPEND,0,p,"192.168.0.1","10.10.10.10","255.255.255.255",
+		"0.0.0.0",0,137,138,80,0,"udp","INPUT","ACCEPT","filter");
+	case 2:
+	return do_init(handle,APPEND,0,p,"0.0.0.0","0.0.0.0","0.0.0.0",
+		"0.0.0.0",0,0,137,0,0,"tcp","INPUT","ACCEPT","filter");
+	case 3:
+	return do_init(handle,APPEND,0,p,"192.168.0.1","15.15.15.15","255.255.255.255",
+		"255.255.255.255",123,128,300,301,0,"tcp",NULL,"ACCEPT","filter");
+	case 4:
+	return do_init(handle,CLEAN,0,p,"0.0.0.0","0.0.0.0","0.0.0.0",
+		"0.0.0.0",0,0,0,0,0,NULL,NULL,NULL,"filter");
+	case 5:
+	return do_init(handle,DELETE,2,p,"0.0.0.0","0.0.0.0","0.0.0.0",
+		"0.0.0.0",0,0,0,0,0,NULL,"INPUT",NULL,"filter");
+	case 6:
+	return do_init(handle,ALTER,2,p,"15.15.15.15","0.0.0.0","0.0.0.0",
+		"0.0.0.0",0,0,0,0,0,"tcp","INPUT","DROP","filter");
+	case 7:
+	return do_init(handle,APPEND,0,p,"222.222.222.222","0.0.0.0","255.255.128.0",
+		"0.0.0.0",0,0,137,0,1,"tcp","INPUT","ACCEPT","filter");
+	case 8:
+	return do_init(handle,APPEND,0,p,"111.111.111.111","123.123.123.123","255.0.0.0",
+		"255.254.0.0",0,0,155,0,2,"tcp","INPUT","ACCEPT","filter");
+	case 9:
+	return do_init(handle,APPEND,0,p,"233.233.1.1","55.55.55.55","255.0.0.0",
+		"255.254.0.0",0,0,0,0,2,NULL,"INPUT","ACCEPT","filter");
+	case 10:
+	return do_init(handle,ALLIN,0,p,"0.0.0.0","0.0.0.0","0.0.0.0",
+		"0.0.0.0",0,0,0,0,0,NULL,NULL,NULL,NULL);
+	case 11:
+	return do_init(handle,APPEND,0,p,"0.0.0.0","0.0.0.0","0.0.0.0",
+		"0.0.0.0",0,8080,0,0,0,NULL,NULL,NULL,"filter");
+	case 12:
+	return do_init(handle,APPEND,0,p,"0.0.0.0","0.0.0.0","0.0.0.0",
+		"0.0.0.0",0,8080,0,0,0,"udp",NULL,NULL,"filter");
+	case 13:
+	return do_init(handle,APPEND,0,p,"0.0.0.0","255.256.257.258","0.0.0.0",
+		"0.0.0.0",0,8080,0,0,0,"udp","FORWARD",NULL,"filter");
+	case -1:
+		exit(0);
+	default:
+		fprintf(stderr,"nothing to do.\n");		
+		return 0;
+}
 
 }
 
 static void 
 send_message_to_my_mod()
 {
-	int    sockfd;
-    struct sockaddr_in    servaddr;
-	
-	if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+int    sockfd;
+struct sockaddr_in    servaddr;
+if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 	    printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
 	    exit(0);
 	}
@@ -129,28 +176,30 @@ send_message_to_my_mod()
     printf("inet_pton error\n");
     exit(0);
     }
-
     if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
     printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
     exit(0);
     }
 
-   // while(1){
-	    int needSend=sizeof(struct handle_c); 
-	/** fatal question: why handle must be malloced  after buffer?????
-	*   struct handle_c *handle =(struct handle_c *) malloc(needSend);
-	*   init_handle(handle);
-	**/
-	    char *buffer=(char*)malloc(needSend);
-	    //handle malloced after buffer,everything is ok.
-	    struct handle_c *handle =(struct handle_c *) malloc(needSend);
-	    
-	    init_handle(handle);
-	  
-	    int pos=0;
-	    int len;
+while(1){
+	int needSend=sizeof(struct handle_c); 
+	 int pos;
+    int len;
+  
+/** fatal question: why handle must be malloced  after buffer?????
+*   struct handle_c *handle =(struct handle_c *) malloc(needSend);
+*   init_handle(handle);
+**/
+    char *buffer=(char*)malloc(needSend);
+    //handle malloced after buffer,everthing is ok.
+    struct handle_c *handle =(struct handle_c *) malloc(needSend);
+   
+    if(init_handle(handle)){
+	    pos =0;
 	    memcpy(buffer,handle,needSend);
-	    while(pos < needSend){
+	    
+	
+		while(pos < needSend){
 			len = send(sockfd, buffer+pos, BUFFER_SIZE, 0);
 			if (len < 0)
 			{
@@ -158,13 +207,14 @@ send_message_to_my_mod()
 			    break;
 			}
 			pos+=len;
-	    }
-	    free(buffer);
-	    free(handle);
-	    buffer = NULL;
-	    handle = NULL;
-  //  }
-    close(sockfd);
+	       }
+    }
+    free(buffer);
+    free(handle);
+    buffer = NULL;
+    handle = NULL;
+}
+close(sockfd);
 }
 
 static void 
@@ -185,7 +235,7 @@ print_message(struct handle_c * h)
 static void 
 do_server()
 {
-    int    listenfd,connfd;
+	int    listenfd,connfd;
     struct sockaddr_in  servaddr;
 
     if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
@@ -205,12 +255,13 @@ do_server()
         exit(0);
     }
 
-    printf("======waiting for my module's request ======\n");
+ //   printf("======waiting for my module's request ======\n");
     while(1){
         if((connfd = accept(listenfd, (struct sockaddr*)NULL, NULL)) == -1){
             printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
             continue;
         }
+	printf("messages coming from middle module.\n");
             int recvSize = sizeof(struct handle_c); 
             char * buffer = (char*)malloc(recvSize); 
 	    if(!buffer){ 
@@ -232,7 +283,6 @@ do_server()
             } 
             free(buffer); 
             buffer = NULL ;
-
 	print_message(handle);
 	free(handle);
     close(connfd);
@@ -264,7 +314,7 @@ listen_alert_info()
         exit(0);
     }
 	
-	printf("======waiting for my module's alert ======\n");
+	//printf("======waiting for my module's alert ======\n");
     while(1){
         if((connfd = accept(listenfd, (struct sockaddr*)NULL, NULL)) == -1){
             printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
@@ -279,13 +329,11 @@ listen_alert_info()
             int len=0; 
             while(pos<recvSize){ 
                 len = recv(connfd,buffer+pos,BUFFER_SIZE,0); 
-                if(len<=0){ 
-                    perror("recv error! \n"); 
+                if(len<=0)
                     break; 
-                } 
                 pos+=len; 
             } 
-	    printf("%s",buffer);
+	    printf("from middle modle :%s",buffer);
             free(buffer); 
             buffer = NULL ;
     close(connfd);
@@ -305,13 +353,13 @@ main ()
         exit(0);
     }
 
-/*     if(pthread_create(&id2,NULL,(void *)(&listen_alert_info),NULL) == -1)
+     if(pthread_create(&id2,NULL,(void *)(&listen_alert_info),NULL) == -1)
     {
         fprintf(stderr,"pthread_create error!\n");
         exit(0);
     }
-*/
-    pthread_join(id1,&ret);
+
+//    pthread_join(id1,&ret);
     send_message_to_my_mod();
 }
 
